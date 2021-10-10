6547d41a2b2a8300566b8dab05f1491040d1229e
[folivora] <[info@folivora.design]>
2021-10-10 15:22:23 +0900

    t

--- src/page/userInfo.js
+++ src/page/userInfo.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 // import Two from './two';
 
 class UserInfo extends React.Component{
-
   render(){
     return(
       <div>
         this is User Information page<br/>
-        <p>{this.props.location.state.color}</p>
+
+        <input type="hidden" data-color={this.props.location.state.color} />
       </div>
     )
   }
